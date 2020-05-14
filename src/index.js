8c1ab5ce486f7421383732c9a92cb3e571b91c01
Nancy Schrader <katelanschrader7@gmail.com>
2020-05-13 21:44:38 -0400

    redoing everything

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import { render } from "react-dom";
+import Calendar from "react-calendar";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const ReactCalendar = () => {
+  const [date, setDate] = useState(new Date());
+
+  const onChange = date => {
+    setDate(date);
+  };
+
+
+  return (
+    <div>
+      <Calendar showWeekNumbers onChange={onChange} value={date} />
+      {console.log(date)}
+      {date.toString()}
+    </div>
+  );
+
+};
+
+render(<ReactCalendar />, document.querySelector("#root"));
