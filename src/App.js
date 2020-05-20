5025a33decc5e8cddcbf39d4c71086eff711b3d4
Nancy Schrader <katelanschrader7@gmail.com>
2020-05-19 20:25:55 -0400

    finished basic styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'react-calendar/dist/Calendar.css';
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { render } from "react-dom";
 import Calendar from "react-calendar";
 import 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 const ReactCalendar = () => {
   const [date, setDate] = useState(new Date());
