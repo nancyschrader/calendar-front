fca55a9506c3d77b309ebab2c280d13cebda1d45
Nancy Schrader <katelanschrader7@gmail.com>
2020-06-10 18:45:13 -0400

    Trying to pull from backend

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  state = {
+    events: []
+  }
+  componentDidMount() {
+    this.getEvents()
+  }
   getEvent = () => {
     fetch('/events')
       .then(response => response.json())
-      .then(json => this.setState({events: json}))
+      .then(json => this.setState({ events: json }))
       .catch(error => console.error(error))
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className='container'>
+          <Header />
+          <Aside />
+          <Main events={this.state.events} />
+          <Nav />
+          <Footer />
+        </div>
+      </div>
+    )
   }
 }
 
